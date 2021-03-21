87bd2b05022980a89f92ca01708d5e91cc78a5fe
Johnny0n <60975096+Johnny0n@users.noreply.github.com>
2021-03-21 23:41:09 +0100

    All Files

---
+++ kontakt.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Main Info -->
+    <title>Kancelaria Prawnicza | Jacek Dżedzyk</title>
+
+	<meta name="keywords" content="kanncelaria,prawnicza,wrocław,wroclaw,kancelaria prawicza wrocław,kancelaria prawicza wroclaw,peritus,peritus wrocław ,dzedzyk,jacek dzedzyk" />
+	<meta name="description" content=" Jacek Dżedzyk| Kancelaria Prawna | zajmuje się obsługą prawną przedsiębiorców, jednostek sektora finansów publicznych, w tym również jednostek samorządu terytorialnego oraz świadczy usługi na rzecz klientów indywidualnych. " />
+	<meta name="author" content="Jan Szarwayn, janek.szarwaryn@gmail.com">
+
+	<!-- inplementacja zewnetrzna -->
+<link rel="shortcut icon" href="img/favicon.png">
+
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	
+<link href='http://fonts.googleapis.com/css?family=Marcellus&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Marcellus+SC&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+	<!-- sklrypt do ikonek  -->
+	<script type="text/javascript" src="js/ReturnToTop.js"></script>
+
+</head>
+<body>
+
+	<button onclick="ReturnToTop()" id="powrot" class="d-md-none" title="Powrót na górę strony">
+	<i class="fas fa-angle-double-up"></i>
+	</button>
+
+	<div id="telefon" class="d-md-none" title="Zadzwoń teraz.">
+		<a href="tel:+48 602 611 137" title="Zadzwoń teraz" style="color:#ffffff;"><i class="fas fa-phone"></i></a>
+	</div>
+	
+<section id="navigation">
+<div class="navbar navbar-default navbar-fixed-top " role="navigation">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle " data-toggle="collapse" data-target=".navbar-collapse">
+<span class="sr-only">Wlacz Nawigacje</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="index.html"><img src="img/logo-white.png" class="img-responsive" class="logoX" alt="logo-white"></a>
+</div>
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav navbar-right scrollto">
+<li><a href="index.html"><h5>Główna</h5></a></li>
+<li><a href="index.html#about"><h5>O Kancelarii</h5></a></li>
+<li><a href="oferta.html"><h5>Oferta</h5></a></li>
+<li><a href="zespol.html"><h5>Zespół</h5></a></li>
+<li><a href="zasady.html"><h5>Zasady</h5></a></li>
+<li><a href="kontakt.php"><h5>Kontakt</h5></a></li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+<section id="home">
+<div class="home-section">
+<div class="container">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-2">
+<div class="center-heading text-center">
+<h1>KONTAKT Z KANCELARIA</h1>
+<h2 class="subtittle">Wyślij do nas wiadomość</h2>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section id="contact">
+<div class="contact-section">
+<div class="container">
+<div class="contact-title">
+<div class="row">
+<div class="col-sm-12 text-center">
+<h2>SKONTAKTUJ SIĘ Z NAMI</h2>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-5">
+<div class="contact-list">
+<ul>
+<li>MIASTO: Wrocław</li>
+<li>BIURO:  ul. Sudecka 74 lok. 21</li>
+<li>KOD: 53-129</li>
+<li>TEL. 602 611 137</li>
+<li>E-MAIL: dzedzyk@op.pl</li>
+
+</ul>
+</div>
+</div>
+<div class="col-sm-7">
+<div class="contact_form">
+<div id="show_contact_msg"></div>
+
+<form method="post" id="contact_form" action="kontakt.php">
+<input type="text" id="contact_name" name="name" class="form-control contact_input_box" placeholder="Imię i Nazwisko" required />
+<input type="email" id="contact_email" name="email" class="form-control contact_input_box" placeholder="E-mail" required />
+<textarea id="contact_text" name="message" rows="6" cols="30" class="form-control contact_input_box" placeholder="Wiadomość" required></textarea>
+<button type="submit" class="btn btn-md btn-block btn-theme-white4"> </i>Wyślij Wiadomość</button>
+</form>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section id="google-map" class="map">
+<container class="gm">
+<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2503.3561486499434!2d17.056569016088066!3d51.13878317957626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9baeaff62d5%3A0xe956b6c641c43959!2saleja+Jana+Kasprowicza+102%2C+51-146+Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1547249413801">
+                </iframe>
+				
+<br />
+<small>
+<a href="src%3d.html" https: www.google.com maps embed?pb="!1m14!1m12!1m3!1d96711.44545930377!2d-73.96430433772667!3d40.75690703760583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1445178937875">
+</a>
+</small>
+</iframe>
+</container>
+</section>
+<section id="footer">
+<div class="container">
+<div class="footer">
+<div class="row">
+
+<div class="col-sm-12 text-center">
+<div class="copy">
+<p>&copy; Copyright 2019 Peritus.</p>
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+</section>
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/jquery.min.js"></script>
+
+<script src="js/jquery-1.11.1.js" type="text/javascript"></script>
+
+<script src="js/bootstrap.js"></script>
+<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+<script src="js/jquery.easing.1.3.min.js" type="text/javascript"></script>
+<script src="js/jquery.sticky.js" type="text/javascript"></script>
+<script>
+        $('#carousel-example').carousel({
+            interval: 4000 //TIME IN MILLI SECONDS
+        })
+        </script>
+		
+	<script type="text/javascript">
+		$('.navbar-nav a').on('click', function() 	{
+			$('.navbar-toggle').click();
+		})
+/*		$ if ('.navbar-collapse).collapse('show()'*/
+	</script>
+<script>
+                    $(function() {
+                $('a[href*=#]:not([href=#])').click(function() {
+                    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                        if (target.length) {
+                            $('html,body').animate({
+                                scrollTop: target.offset().top -89
+                            }, 1000);                            
+                            return false;
+                        }
+                    }
+                });
+            });
+        </script>
+<script>
+
+	$("#subscriber_form").submit(function(e)
+	{
+	$('#show_subscriber_msg').html('<div class=gen>Submiting..</div>');
+	var subscriberemail = $('wilk@wdwkancelaria.pl').val();
+		var formURL = $(this).attr("action");
+		var data = {
+					subscriberemail:subscriberemail
+				   }
+		$.ajax(
+		{
+			url : formURL,
+			type: "POST",
+			data : data,
+			
+		 success: function (res) {
+								 if(res=='1'){
+									$('#show_subscriber_msg').html('<div class=gen>Dziękujemy, odpowiemy na twoje pytanie w najbliższym czase.</div>');
+								 }
+					
+								 if(res=='5'){
+								 $('#show_subscriber_msg').html('<div class=err>Proszę wpisać aktualny adres e-mail</div>');
+								 }
+							}
+		});
+		e.preventDefault();	// stop skryptu
+	});
+	/* ==============================================
+	Formularz kontaktowy
+	=====================================================================*/	
+	$("#contact_form").submit(function(e)
+	{
+	$('#show_contact_msg').html('<div class=gen>Zatwierdzanie..</div>');
+	var username = $('#contact_name').val();
+	var useremail = $('#contact_email').val();
+	var commenttext = $('#contact_text').val();
+		var formURL = $(this).attr("action");
+		var data = {
+					username:username,
+					useremail:useremail,
+					commenttext:commenttext,
+				   }
+		$.ajax(
+		{
+			url : formURL,
+			type: "POST",
+			data : data,
+			
+		 success: function (res) {
+								 if(res=='1'){
+									$('#show_contact_msg').html('<div class=gen>Dziękuję bardzo, Twoja wiadomość została wysłana!</div>');
+								 }
+					
+								 if(res=='5'){
+								 $('#show_contact_msg').html('<div class=err>Proszę wpisać aktualny adres e-mail</div>');
+								 }
+							}
+		});
+		e.preventDefault();	//stop skryptu
+	});
+	</script>
+</body>
